4d310bceaa8227bd24ecf46da992ca5f902c72ae
TarunAnur <47273464+TarunAnur@users.noreply.github.com>
2020-09-17 15:50:55 +0530

    Create input2.c

---
+++ Parser/Sample-Programs/input2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int n = 10;
+    if(n % 2 == 0)
+    {
+        printf("EVEN NUMBER\n");
+    }
+    else
+    {
+        printf("ODD NUMBER\n");
+    }    
+}
