f2f74bd9e86c68dfa4a0288004eaa764b035b4d4
shashank68 <shashankindiamanoj@gmail.com>
2020-10-20 18:22:18 +0530

    new dir

---
+++ SyntaxPhase/Sample-Programs/input2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int n = 0;
+    if(n % 2 == 0)
+    {
+        printf("EVEN NUMBER\n");
+    }
+    else
+    {
+        printf("ODD NUMBER\n");
+    }    
+}
